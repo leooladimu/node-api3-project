43987f6659f0de8ad734e9faab947cb7d697ce5a
Leo Oladimu <leooladimu9@gmail.com>
2022-02-09 22:06:45 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const logger = require('morgan');
-const usersRouter = require('./users/users-router');
+const { logger } = require('./middleware/middleware');
 const server = express();
 
 server.use(express.json());
-server.use('/api/hubs', usersRouter);
 
-// remember express by default cannot parse JSON in request bodies
+const usersRouter = require('./users/users-router');
 
-// global middlewares and the user's router need to be connected here
+server.use(logger);
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use('*', (req, res) => {
+  res.status(404).json({message: `${req.method} ${req.baseUrl} not found!`})
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: process.env.NODE_ENV === 'PROD' ? 'sorry, there was an error' : err.message,
+  })
 });
 
 module.exports = server;
